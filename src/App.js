6a5c123ccfbb0bcb0722f36fb7fddc327a553c25
ihab-sensei <ib-mashhadani@hotmail.com>
2020-09-30 12:03:02 +0300

    Created base react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Do you even lift bro?!</h1>
+    </div>
+  );
+}
+
+export default App;
